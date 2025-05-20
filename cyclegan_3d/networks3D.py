4b15aa915707507505f5975528f0f5be88512a4a
Leeyoungsup <liive29252@gmail.com>
2025-05-20 11:02:43 +0900

    ResnetGenerator의 블록 수를 6에서 5로 변경하여 모델 구조 수정

--- cyclegan_3d/networks3D.py
+++ cyclegan_3d/networks3D.py
@@ -79,7 +79,7 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
     if netG == 'resnet_9blocks':
         net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9)
     elif netG == 'resnet_6blocks':
-        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6)
+        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=5)
     elif netG == 'unet_custom':
         net = UnetGenerator(input_nc, output_nc, 5, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'unet_128':
